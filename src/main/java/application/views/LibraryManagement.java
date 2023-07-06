b320bf41d1f1c665b7ac1ea873768afbb803dd8f
Khang Nguyễn <khang.nguyen@realstake.io>
2023-07-06 22:03:26 +0700

    feat(app): init swing UI

--- src/main/java/application/views/LibraryManagement.java
+++ src/main/java/application/views/LibraryManagement.java
@@ -2,36 +2,72 @@ package application.views;
 
 import application.controllers.DocumentController;
 import application.controllers.UserController;
-import application.models.document.Document;
-import application.models.user.User;
+import application.views.screens.BaseScreen;
 
-import java.util.Date;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LibraryManagement {
-    private UserController userController;
-    private DocumentController documentController;
+public class LibraryManagement extends BaseScreen implements ActionListener {
+    private final UserController userController;
+    private final DocumentController documentController;
 
     public LibraryManagement() {
-
+        super();
         userController = new UserController();
         documentController = new DocumentController();
     }
 
     public void start() {
-        this.login();
+        this.visible();
     }
 
-    private void login() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to library management!");
-//        System.out.println("Enter your ID to sign in: ");
-//        String userId = scanner.nextLine();
-//        User user = userController.findOneById(userId);
-//        System.out.println(user);
+    public void visible() {
+        this.showLogin();
+    }
 
-        System.out.println("===========");
-        Document document = documentController.findOneDocumentByName("The Why");
-        System.out.println(document);
+    public void showLogin() {
+        JLabel label = new JLabel("Library management");
+        label.setBounds(1024 / 2 - 80, 720 / 2 - 40, 160, 30);
+
+        JTextField textField = new JTextField("User id");
+        textField.setBounds(1024 / 2 - 100, 720 / 2 - 10, 180, 30);
+
+        JButton button = new JButton("Login");
+        button.setBounds(1024 / 2 - 100, 720 / 2 + 20, 180, 30);
+
+        button.addActionListener(e -> {
+            String userInput = textField.getText();
+            System.out.println(userInput);
+            this.frame.getContentPane().removeAll();
+            this.frame.getContentPane().repaint();
+            this.showMain();
+        });
+
+        this.frame.getContentPane().add(textField);
+        this.frame.getContentPane().add(label);
+        this.frame.getContentPane().add(button);
+    }
+
+    public void showMain() {
+        JLabel label = new JLabel("You want to find a book");
+        label.setBounds(1024 / 2, 720 / 2 - 40, 200, 30);
+
+        JTextField textField = new JTextField("The why");
+        textField.setBounds(1024 / 2, 720 / 2 - 10, 180, 30);
+
+        JButton button = new JButton("find");
+        button.setBounds(1024 / 2, 720 / 2 + 20, 180, 30);
+
+        this.frame.getContentPane().add(textField);
+        this.frame.getContentPane().add(label);
+        this.frame.getContentPane().add(button);
+        this.frame.getContentPane().revalidate();
+        this.frame.getContentPane().repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
